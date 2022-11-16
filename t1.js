114955deae5fd34eb3610713d9eab1abe6939a45
Kshiteesh Kandari <kshiteeshkandari@gmail.com>
2022-11-16 05:32:16 -0600

    checking if the pages are loading correctly

--- t1.js
+++ t1.js
@@ -1,4 +1,4 @@
-var p = ["Shit! I am never going to make it!","Hey wassup? you seem in a hurry!","Yeah! my dad is at the airport and the AI taxi won't accpet my card! I really need to see him! haven't seen him in years","I own a taxi, I can take you there.","What? Human Driven taxi's exist? I thought ever since the AI took over the industry I thought people werent driving anymore!","Yeah, its been a rough couple years. I used to be best taxi driver in Yorknew City but ever since Automated cars became a thing I barely get a ride anymore.","Thats crazy! it totally skipped my mind how Automated Driving could have affected people in the industry","Yeah they started Automated Driving with short haul truck driving, we thought out jobs were still safe but in just 3 years it took overtaxi service, food delivery and now there is no kind of driving that would earn me any bread.","I have been so focused on finding my dad, I didnt notice how people were being affected by Automated Driving!","that remind me, your dad is still at the Airport right!","ahhh shit! how did i get lost in this conversation","dont worry I can take you there! remember I used to be the best taxi driver in all of Yorknew","That would be great can you take me there in 10 minutes","You bet!"];
+var p = ["Shit! I am never going to make it!","Hey wassup? you seem in a hurry!","Yeah! my dad is at the airport and the AI taxi won't accpet my card! I really need to see him! I haven't seen him in years","I own a taxi, I can take you there.","What? Human Driven taxi's exist? I thought ever since the AI took over driving, people weren't driving anymore!","Yeah, its been a rough couple years. I used to be best taxi driver in Yorknew City but ever since Automated cars became a thing I barely get a ride anymore.","Thats crazy! I was so hyped about AI driven cars that I didnt think how Automated Driving could have affected people in the Automobile Industry","Yeah they started Automated Driving with short haul truck driving, we thought some of our jobs were still safe but in just 3 years it took over taxi service, food delivery and now there is no kind of driving that would earn me any bread.","I have been so focused on finding my dad, I didnt bother to look around and think that people would actually lose jobs beause of AI","that reminds me, your dad is still at the Airport right!","ahhh shit! how did i get lost in this conversation","dont worry I can take you there! remember I used to be the best taxi driver in all of Yorknew","That would be great can you take me there in 10 minutes","You bet!"];
 
 //sounds
 var Yes;
@@ -14,7 +14,7 @@ function preload(){
   conV = loadImage('/media/convoBuble.png');
   us = loadImage('/media/us.png');
   Yes = loadSound("/media/Sounds/yes.mp3");
-  crap = loadSound("/media/Sounds/crap.mp3");
+  crap = loadSound("./media/Sounds/crap.mp3");
   fast = loadSound("/media/Sounds/.mp3");
 }
 
